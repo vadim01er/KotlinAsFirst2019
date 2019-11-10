521f2e7f9dfa587ebe2bb3836d9aad1326d3a3c3
Vadim Ershov <vadim.ershov.01@mail.ru>
2019-11-10 20:09:37 +0500

    7 started

--- src/lesson7/task1/Files.kt
+++ src/lesson7/task1/Files.kt
@@ -368,7 +368,7 @@ Suspendisse <s>et elit in enim tempus iaculis</s>.
  * (Отступы и переносы строк в примере добавлены для наглядности, при решении задачи их реализовывать не обязательно)
  */
 fun markdownToHtmlSimple(inputName: String, outputName: String) {
-    
+    TODO()
 }
 
 /**
